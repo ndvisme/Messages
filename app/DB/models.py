883ff018e599d5c8a79066a826d6875765764180
Stav Tasa <stavtasa@gmail.com>
2023-06-01 13:33:27 +0300

    add json to db table and change enum to string

--- app/DB/models.py
+++ app/DB/models.py
@@ -1,16 +1,22 @@
 from app import db
-import enum
-class repeat(enum.Enum):
-    daily = 1
-    weekly = 2
-    monthly = 3
+
+
 class Message(db.Model):
     __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
-    repeat = db.Column(db.Enum(repeat), nullable=False)
+    repeat = db.Column(db.String(100), nullable=True)
     dest_groups_id = db.Column(db.String(200), nullable=False)
     time_to_send = db.Column(db.DateTime(120), nullable=False)
     message_data = db.Column(db.String(500), nullable=False)
     message_title = db.Column(db.String(80), nullable=True)
+
+    def json(self):
+        return {'id': self.id, 
+                'user_id': self.user_id,
+                'repeat': self.repeat,
+                'dest_groups_id': self.dest_groups_id, 
+                'time_to_send': self.time_to_send,
+                'message_data': self.message_data,
+                'message_title': self.message_title}
